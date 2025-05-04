3d287cc682bfbf69e2a754bb7f95efb079673b87
MarxMad <gerardo.gerry.pedrizco@gmail.com>
2025-05-04 01:18:54 -0600

    vercel en blanco, espero ya se arregle

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,10 @@ export default defineConfig(({ mode }) => {
             recharts: 'Recharts'
           }
         }
-      }
-    }
+      },
+      chunkSizeWarningLimit: 1600,
+      sourcemap: true
+    },
+    base: '/'
   }
 })
